ad4033cd2666922ec9bfc60d6f70c0451a895a24
Klara Wesselkamp <klara.wesselkamp@cofinpro.de>
2021-06-11 16:19:30 +0200

    e

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,19 +3,48 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from os import listdir
+import numpy as np
+import torch
+import torchvision
+import kaggle
+
+from make_files import make_target_tensors
+    
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    
+    input_filepath = '../../data/raw'
 
-
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files('andrewmvd/sheep-detection', path=input_filepath, unzip=True)
+    
+    #transforming xml-files to txt files (using function in folder)
+    # txt-files will be put in the interim-folder
+    make_target_tensors()
+    
+    
+    # get list of all images in dataset
+    #image_files_list = listdir(input_filepath)
+    
+    #for im in image_files_list:
+    #    file_path = input_filepath + '/' + im
+    #    x_rgb: torch.tensor = torchvision.io.read_image(file_path)  # CxHxW / torch.uint8
+    #    x_rgb = x_rgb.unsqueeze(0)  # BxCxHxW
+        
+        #resize the images so that they all have the same size
+        
+    
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
