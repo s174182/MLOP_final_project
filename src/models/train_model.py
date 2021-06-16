8d8982f03d4c604924a0f6ee2c83cc9216f0c355
mariaGarofalakis <mariagarofal@gmail.com>
2021-06-16 09:15:20 +0200

    training works!!!

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -25,6 +25,9 @@ import transforms as T
 import utils
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use( 'tkagg')
 
 
 
@@ -84,7 +87,7 @@ class TrainOREvaluate(object):
 
 
     def object_detection_api(boxes, pred_cls, threshold=0.5, rect_th=3, text_size=3, text_th=3):
-        img = cv2.imread('C:/Users/maria/Desktop/mathimata/dtu_mlops/project/maxresdefault.jpg')
+        img = cv2.imread('C:/Users/maria/Desktop/mathimata/dtu_mlops/project/photo-1588942173353-0c53a1bf9081.jpg')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
@@ -119,42 +122,39 @@ class TrainOREvaluate(object):
 
 
 
-        data_loader = torch.utils.data.DataLoader(my_dataset, batch_size=24, shuffle=True,collate_fn=utils.collate_fn)
+      #  data_loader = torch.utils.data.DataLoader(my_dataset, batch_size=24, shuffle=True,collate_fn=utils.collate_fn)
 
+        data_loader = torch.utils.data.DataLoader(
+            my_dataset, batch_size=2, shuffle=True, num_workers=4,
+            collate_fn=utils.collate_fn)
 
 
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
-
-        # Define RPN
-        anchor_generator = AnchorGenerator(sizes=tuple([(16, 32, 64, 128, 256) for _ in range(5)]),
-                                           # let num of tuple equal to num of feature maps
-                                           aspect_ratios=tuple([(0.75, 0.5, 1.25) for _ in range(
-                                               5)]))  # ref: https://github.com/pytorch/vision/issues/978
-        rpn_head = RPNHead(256, anchor_generator.num_anchors_per_location()[0])
-        model.rpn = RegionProposalNetwork(
-            anchor_generator=anchor_generator, head=rpn_head,
-            fg_iou_thresh=0.7, bg_iou_thresh=0.3,
-            batch_size_per_image=48,  # use fewer proposals
-            positive_fraction=0.5,
-            pre_nms_top_n=dict(training=200, testing=100),
-            post_nms_top_n=dict(training=160, testing=80),
-            nms_thresh=0.7
-        )
-
-        num_classes = 2  # 1 class (sheep) + background
-
+        # replace the classifier with a new one, that has
+        # num_classes which is user-defined
+        num_classes = 2  # 1 class (person) + background
         # get number of input features for the classifier
-        in_features = model.roi_heads.box_predictor.cls_score.in_features  # get number of features
-        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes=num_classes)
-        model.roi_heads.fg_bg_sampler.batch_size_per_image = 24
-        model.roi_heads.fg_bg_sampler.positive_fraction = 0.5
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        # replace the pre-trained head with a new one
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+        model.to(device)
+
+
 
         params = [p for p in model.parameters() if p.requires_grad]
-        optimizer = torch.optim.Adam(params, lr=0.0005, betas=(0.9, 0.999), weight_decay=0.0005)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        optimizer = torch.optim.SGD(params, lr=0.005,
+                                    momentum=0.9, weight_decay=0.0005)
+
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                       step_size=3,
+                                                       gamma=0.1)
+
         metric_collector = []
-        num_epochs = 15
+        num_epochs = 5
+
 
         for epoch in range(num_epochs):
             # train for one epoch, printing every 5 iterations
@@ -165,44 +165,47 @@ class TrainOREvaluate(object):
             # Evaluate with validation dataset
        #     evaluate(model, data_loader_test, device=device)
             # save checlpoint
-            torch.save(model.state_dict(),  '../../models/sheep_train.pth')
+        torch.save(model.state_dict(),  '../../models/sheep_train.pth')
 
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
         parser = argparse.ArgumentParser(description='Evaluating arguments')
-        parser.add_argument('--load_model_from', default="checkpoint.pth")
+        parser.add_argument('--load_model_from', default='../../models/sheep_train.pth')
         # add any additional argument that you want
         args = parser.parse_args(sys.argv[2:])
         print(args)
 
-        COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-                                        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-                                        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-                                        'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A',
-                                        'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
-                                        'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                                        'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass', 'cup', 'fork',
-                                        'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli',
-                                        'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant',
-                                        'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop',
-                                        'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                                        'sink', 'refrigerator', 'N/A', 'book', 'clock', 'vase', 'scissors',
-                                        'teddy bear', 'hair drier', 'toothbrush']
+        COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'sheep']
 
+
+        #Evaluation  here
         #Evaluation  here
         if args.load_model_from:
             print("Evaluation here")
 
             print(torch.cuda.is_available())
 
-            images_list=torch.load('../../data/processed/images_list.pt')
-
             model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+            # replace the classifier with a new one, that has
+            # num_classes which is user-defined
+            num_classes = 2  # 1 class (person) + background
+            # get number of input features for the classifier
+            in_features = model.roi_heads.box_predictor.cls_score.in_features
+            # replace the pre-trained head with a new one
+            model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+
+
+
+
+            model.load_state_dict(torch.load(args.load_model_from))
             model.eval()
 
             threshold = 0.5
 
-            img = Image.open('C:/Users/maria/Desktop/mathimata/dtu_mlops/project/maxresdefault.jpg').convert('RGB')
+            img = Image.open('C:/Users/maria/Desktop/mathimata/dtu_mlops/project/photo-1588942173353-0c53a1bf9081.jpg').convert('RGB')
 
 
             x = TF.to_tensor(img)  # use kornia function to import?
