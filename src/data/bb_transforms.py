557fc21d12101d8567a0edb9c249bd484366063b
Klara Wesselkamp <klara.wesselkamp@cofinpro.de>
2021-06-17 10:22:24 +0200

    hasklfdhlk

--- src/data/bb_transforms.py
+++ src/data/bb_transforms.py
@@ -9,9 +9,8 @@ import torch
 
 def createBinImgFromBB (image, annotation):
     bb_coords=annotation.get('boxes')
-    binImg = np.zeros((image.size()[2], image.size()[3]))    
-    binImg[int(bb_coords[1].item()):int(bb_coords[3].item()), int(bb_coords[0].item()):int(bb_coords[2].item())]=1
-    
+    binImg = np.zeros((image.size()[2], image.size()[3]))   
+    binImg[int(bb_coords[1].item()):int(bb_coords[3].item()), int(bb_coords[0].item()):int(bb_coords[2].item())]=1    
     return torch.from_numpy(binImg)
 
 
@@ -20,10 +19,9 @@ def retrieveBBfromBinImg (image):
     colsum = np.sum(binImg, axis=0)
     rowsum = np.sum(binImg, axis=1)
 
-    y1 = np.nonzero(colsum)[0][0]
-    y2 = np.nonzero(colsum)[0][-1] + 1
-    x1 = np.nonzero(rowsum)[0][0]
-    x2 = np.nonzero(rowsum)[0][-1] + 1
-        
+    x1 = np.nonzero(colsum)[0][0]
+    x2 = np.nonzero(colsum)[0][-1] + 1
+    y1 = np.nonzero(rowsum)[0][0]
+    y2 = np.nonzero(rowsum)[0][-1] + 1
     return torch.from_numpy(np.array([x1, y1, x2, y2]))
 
