4feaf4c8edbc5d8897122322151c57b3306fd983
JorgeSintes <jorge.sintes@gmail.com>
2021-06-20 20:40:00 +0200

    Finished model trails

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -17,53 +17,28 @@ matplotlib.use( 'tkagg')
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
+class UseModel(object):
 
+    def __init__(self, model, image):
+        if model == 'vanilla':
+            self.path_to_model = '../../models/sheep_vanilla.pth'
+        elif model == 'normal':
+            self.path_to_model = '../../models/sheep_train_normal.pth'
+        elif model == 'augmented':
+            self.path_to_model = '../../models/sheep_train_augmented.pth'
+        else:
+            print('Please select a valid dataset')
+            exit(1)
 
-class TestModels(object):
+        self.path_to_image = '../../data/external/' + image
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    def __init__(self, num_epochs, path_to_folder):
-
-        self.path_to_model = path_to_folder
-        self.num_epochs = num_epochs
-
-
-    def modelEvaluation(self):
-
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-
-        num_classes = 2  # 1 class (person) + background
-
-        in_features = model.roi_heads.box_predictor.cls_score.in_features
-        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-        model.load_state_dict(torch.load(args.load_model_from))
-        model.to(device)
-
-        test_images_list = torch.load('../../data/processed/test/images_test.pt')
-        test_annotation_list = torch.load('../../data/processed/test/annotations_test.pt')
-
-        my_dataset = construct_dataset.constructDataset(test_annotation_list, test_images_list,None)
-
-        data_loader = torch.utils.data.DataLoader(
-            my_dataset, batch_size=2, shuffle=True, num_workers=4,
-            collate_fn=utils.collate_fn)
-
-        for epoch in range(self.num_epochs):
-            evaluate(model, data_loader, device=device)
-
-
-
-    def object_detection_api(boxes, pred_cls, threshold=0.5, rect_th=3, text_size=3, text_th=3):
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        img = cv2.imread('../../data/external/download.jpeg')
+    def object_detection_api(self, boxes, pred_cls, threshold=0.5, rect_th=3, text_size=3, text_th=3):
+        
+        img = cv2.imread(self.path_to_image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-
-
         for i in range(len(boxes)):
-            print(type(boxes[i]))
-            print(type(boxes[i][0][0]))
             cv2.rectangle(img, (boxes[i][0][0].astype(int),boxes[i][0][1].astype(int)), (boxes[i][1][0].astype(int), boxes[i][1][1].astype(int)), color=(0, 255, 0), thickness=rect_th)
             cv2.putText(img, pred_cls[i], (boxes[i][0][0].astype(int), boxes[i][0][1].astype(int)), cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0),thickness=text_th)
         plt.figure(figsize=(20, 30))
@@ -72,58 +47,49 @@ class TestModels(object):
         plt.yticks([])
         plt.show()
 
-
-
-
-    def evaluate_model(self):
+    def use(self, threshold):
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-
-        # replace the classifier with a new one, that has
-        # num_classes which is user-defined
-        num_classes = 2  # 1 class (person) + background
-        # get number of input features for the classifier
+        num_classes = 2
         in_features = model.roi_heads.box_predictor.cls_score.in_features
-        # replace the pre-trained head with a new one
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-        # model.load_state_dict(torch.load(args.load_model_from))
+        model.load_state_dict(torch.load(self.path_to_model, map_location=self.device))
         model.eval()
 
-        threshold = 0.5
-
-        img = Image.open(
-            '../../data/external/download.jpeg').convert('RGB')
+        img = Image.open(self.path_to_image).convert('RGB')
 
         x = TF.to_tensor(img)  # use kornia function to import?
-
+        x = x.reshape(1, *x.shape)
 
         COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'sheep']
 
-        pred = model([x])
+        pred = model(x)
         pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())]
         pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
         pred_score = list(pred[0]['scores'].detach().numpy())
-        pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
+        pred_t = [pred_score.index(thing) for thing in pred_score if thing > threshold]
+        if len(pred_t) == 0:
+            print(f'Selected threshold of {threshold} is too high. Try lowering it')
+            exit(1)
+        pred_t = pred_t[-1]
         pred_boxes = pred_boxes[:pred_t + 1]
         pred_class = pred_class[:pred_t + 1]
 
-        TestModels.object_detection_api(pred_boxes, pred_class, threshold=0.5, rect_th=3, text_size=3, text_th=3)
+        self.object_detection_api(pred_boxes, pred_class, threshold=threshold, rect_th=3, text_size=3, text_th=3)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-load_model_from',
-                        default='../../models/sheep_vanilla.pth',
+    parser.add_argument('-model',
+                        default='augmented',
                         type=str)
-    parser.add_argument('-num_epochs',
-                        default=1,
-                        type=int)
+    parser.add_argument('-image',
+                        default='figure01.jpeg',
+                        type=str)
+    parser.add_argument('-threshold',
+                        default=0.5,
+                        type=float)
     args = parser.parse_args()
 
-    if (args.num_epochs and args.load_model_from):
-        obj_evaluate = TestModels(args.num_epochs,args.load_model_from)
-        obj_evaluate.modelEvaluation()
-
-    else:
-        print('Please provide for a valid model path for evaluation')
-        exit(1)
+    obj_evaluate = UseModel(args.model, args.image)
+    obj_evaluate.use(args.threshold)
