233972c3bc9635a5d58cd4842119c347c03b8309
mariaGarofalakis <mariagarofal@gmail.com>
2021-06-21 14:46:10 +0200

    optuna implementation

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,7 +12,6 @@ from os import listdir
 import torch
 import torchvision
 
-
 from PIL import Image
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
@@ -21,31 +20,46 @@ from torch.utils.data import Dataset
 import construct_dataset
 import transforms as T
 import utils
+import optuna
 
 
 class TrainOREvaluate(object):
 
     def __init__(self):
-        self.lr = args.lr
+        # self.lr = args.lr
         self.num_epochs = args.num_epochs
         self.dataset = args.dataset
-        self.batch_size = args.batch_size
+        # self.batch_size = args.batch_size
         self.train_size = args.train_size
+        self.best_val_loss = float('Inf')
 
-    def train(self):
+    def suggest_hyperparameters(trial):
+        # Learning rate on a logarithmic scale
+        lr = trial.suggest_float("lr", 1e-6, 1e-2, log=True)
+        # Batch size  in the range from 2 to 6 with step size 1
+        batch_size = int(trial.suggest_float("batch_size", 1, 4, step=1))
+
+        return lr, batch_size
+
+    def train(self, trial):
         print("Training day and night")
-     
+
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        lr, batch_size = TrainOREvaluate.suggest_hyperparameters(trial)
+
         print('Training on :', device)
 
         if self.dataset == 'normal':
-            annotation_list = torch.load('../../data/processed/train/annotation_list.pt')
+            annotation_list = torch.load(
+                '../../data/processed/train/annotation_list.pt')
             images_list = torch.load('../../data/processed/train/images_list.pt')
         elif self.dataset == 'augmented':
-            annotation_list = torch.load('../../data/processed/train/annotation_list_augmented.pt')
-            images_list = torch.load('../../data/processed/train/images_list_augmented.pt')
-       
-        train_dataset = construct_dataset.constructDataset(annotation_list,images_list,transform=None)
+            annotation_list = torch.load(
+                '../../data/processed/train/annotation_list_augmented.pt')
+            images_list = torch.load(
+                '../../data/processed/train/images_list_augmented.pt')
+
+        train_dataset = construct_dataset.constructDataset(annotation_list, images_list, transform=None)
 
         train_len = int(self.train_size * len(train_dataset))
         valid_len = len(train_dataset) - train_len
@@ -53,15 +67,15 @@ class TrainOREvaluate(object):
         train_set, validation_set = torch.utils.data.random_split(train_dataset, lengths=[train_len, valid_len])
 
         train_loader = torch.utils.data.DataLoader(
-            train_set, batch_size=self.batch_size, shuffle=True, num_workers=4,
-            collate_fn=utils.collate_fn) # collate_fn allows you to have images (tensors) of different sizes
+            train_set, batch_size=batch_size, shuffle=True, num_workers=4,
+            collate_fn=utils.collate_fn)  # collate_fn allows you to have images (tensors) of different sizes
 
         validation_loader = torch.utils.data.DataLoader(
-            validation_set, batch_size=self.batch_size, shuffle=True, num_workers=4,
-            collate_fn=utils.collate_fn) # collate_fn allows you to have images (tensors) of different sizes
+            validation_set, batch_size=batch_size, shuffle=True, num_workers=4,
+            collate_fn=utils.collate_fn)  # collate_fn allows you to have images (tensors) of different sizes
 
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-        
+
         # replace the classifier with a new one, that has
         # num_classes which is user-defined
         num_classes = 2  # 1 class (sheep) + background
@@ -70,13 +84,12 @@ class TrainOREvaluate(object):
         # replace the pre-trained final layer classification and box regression layers with a new one
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-        torch.save(model.state_dict(),  '../../models/sheep_vanilla.pth')
+        torch.save(model.state_dict(), '../../models/sheep_vanilla.pth')
 
         model.to(device)
 
-
         params = [p for p in model.parameters() if p.requires_grad]
-        optimizer = torch.optim.SGD(params, lr=self.lr,
+        optimizer = torch.optim.SGD(params, lr=lr,
                                     momentum=0.9, weight_decay=0.0005)
 
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
@@ -92,41 +105,86 @@ class TrainOREvaluate(object):
             # update the learning rate
             lr_scheduler.step()
             # Evaluate with validation dataset
-            evaluate(model, validation_loader, device=device)
+            evaluation_result = evaluate(model, validation_loader, device=device)
+            validation_AP_accuracy = evaluation_result.coco_eval.get('bbox').stats[0]
+
+            trial.report(validation_AP_accuracy, epoch)
+            if trial.should_prune():
+                raise optuna.TrialPruned()
+
             # save checkpoint
-            torch.save(model.state_dict(),  '../../models/sheep_train_' + self.dataset + '.pth')
+
+            torch.save(model.state_dict(),
+                       '../../models/sheep_train_' + self.dataset + '.pth')
 
         # Creating the test set and testing
-        annotation_list = torch.load('../../data/processed/test/annotations_test.pt')
+        annotation_list = torch.load(
+            '../../data/processed/test/annotations_test.pt')
         images_list = torch.load('../../data/processed/test/images_test.pt')
-       
-        test_dataset = construct_dataset.constructDataset(annotation_list,images_list,transform=None)
+
+        test_dataset = construct_dataset.constructDataset(annotation_list, images_list, transform=None)
 
         test_loader = torch.utils.data.DataLoader(
-            test_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4,
-            collate_fn=utils.collate_fn) # collate_fn allows you to have images (tensors) of different sizes
+            test_dataset, batch_size=batch_size, shuffle=True, num_workers=4,
+            collate_fn=utils.collate_fn)  # collate_fn allows you to have images (tensors) of different sizes
 
-        evaluate(model, test_loader, device=device)
+        evaluation_result = evaluate(model, test_loader, device=device)
+
+        final_accuracy = evaluation_result.coco_eval.get('bbox').stats[0]
+
+        if final_accuracy <= self.best_val_loss:
+            self.best_val_loss = final_accuracy
+            print(self.best_val_loss)
+
+        return final_accuracy
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-lr',
-                        default=0.005,
-                        type=float)
+    # parser.add_argument('-lr',
+    #                     default=0.005,
+    #                     type=float)
     parser.add_argument('-num_epochs',
-                        default=5,
+                        default=2,
                         type=int)
     parser.add_argument('-dataset',
                         default='augmented',
                         type=str)
-    parser.add_argument('-batch_size',
-                        default=2,
-                        type=int)
+    #  parser.add_argument('-batch_size',
+    #                      default=4,
+    #                      type=int)
     parser.add_argument('-train_size',
                         default=0.9,
                         type=float)
     args = parser.parse_args()
 
     trainObj = TrainOREvaluate()
-    trainObj.train()
+    #    final_accuracy = trainObj.train(lr)
+
+    study = optuna.create_study(study_name="Final-project-optuna", direction="maximize",
+                                pruner=optuna.pruners.MedianPruner(
+                                    n_startup_trials=5, n_warmup_steps=1
+                                ))
+    study.optimize(trainObj.train, n_trials=20)
+
+    # Initialize the best_val_loss value
+    mean_AP_accuracy = best_val_loss = float('Inf')
+
+    if mean_AP_accuracy <= best_val_loss:
+        best_val_loss = mean_AP_accuracy
+
+    # Print optuna study statistics
+    print("\n++++++++++++++++++++++++++++++++++\n")
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Trial number: ", trial.number)
+    print("  Loss (trial value): ", trial.value)
+
+    print("  Params: ")
+    lr = 0
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
